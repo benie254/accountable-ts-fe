8755a76b61c9bf48ab9f742e701c265faa383b35
benie254 <davinci.monalissa@gmail.com>
2025-01-01 21:32:55 +0300

    init (app): initialize project with setup files and directories

---
+++ accountable/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Accountable</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla error
+        nobis perspiciatis quos distinctio ea amet ex quo consectetur hic
+        sapiente doloremque facere consequatur, ab doloribus quia, asperiores
+        dicta nostrum.
+      </p>
+    </>
+  );
+}
+
+export default App;
